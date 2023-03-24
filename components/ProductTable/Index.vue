94fd78d0fca7f6c15a44a6beedd7a64ab1749ac6
Carlos <carlosmanueldu@gmail.com>
2023-03-24 19:54:17 -0300

    Completando el componente de tabla, configurando función para imprimir PDF

---
+++ components/ProductTable/Index.vue
@@ -0,0 +1,49 @@
+<template>
+    <section>
+        <ProductTableMainTable :products="products" ref="mainTable"/>
+        <button @click="createPDF">Crear pdf</button>
+    </section>
+</template>
+
+<script setup>
+    const props = defineProps({
+        products: {
+            type: Array,
+            required: true
+        }
+    })
+    const mainTable = ref()
+
+    function createPDF(){
+        // let table = document.getElementsByTagName('table')[0].cloneNode(true)
+        let table = mainTable.value.getClone()
+        let style =  `
+            <style>
+                table {width: 100%;font: 17px Calibri;}
+                table, th, td {border: solid 1px #DDD; border-collapse: collapse;
+                padding: 2px 3px;text-align: center;}
+            </style>
+        `
+        // CREATE A WINDOW OBJECT.
+        let tableWindow = window.open('', '', 'height=700,width=700');
+
+        tableWindow.document.write(`
+            <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Document</title>
+                </head>
+                <body>
+                </body>
+            </html>
+        `)
+        tableWindow.document.head.innerHTML += style
+        tableWindow.document.body.append(table)
+        
+        tableWindow.print();    // PRINT THE CONTENTS.
+        tableWindow.close(); 	// CLOSE THE CURRENT WINDOW.
+    }
+</script>
---
+++ components/ProductTable/MainTable.vue
@@ -0,0 +1,54 @@
+<template>
+    <table ref="table">
+        <thead>
+            <tr>
+                <th>Imagen</th>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Cantidad</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="{ imageData, name, price, quantity } in props.products" :key="name">
+                <td>
+                    <img :src="imageData" alt="" width="90">
+                </td>
+                <td>{{ name }}</td>
+                <td>{{ price }}</td>
+                <td>{{ quantity }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script setup>
+    const props = defineProps({
+        products: {
+            type: Array,
+            required: true
+        }
+    })
+    const table = ref(null)
+
+    function getClone() {
+        return table.value.cloneNode(true)
+    }
+
+    defineExpose({
+        getClone,
+    })
+</script>
+
+<style scoped>
+    table {
+        width: 100%;
+        font: 17px Calibri;
+    }
+
+    table, th, td {
+        border: solid 1px #DDD;
+        border-collapse: collapse;
+        padding: 2px 3px;
+        text-align: center;
+    }
+</style>
