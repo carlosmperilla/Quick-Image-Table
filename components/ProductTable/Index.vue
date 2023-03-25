e4a5bd25570a0f007a892a7900740cf06f2d3862
Carlos <carlosmanueldu@gmail.com>
2023-03-25 04:28:50 -0300

    Arreglando estilos de exportación de pdf

--- components/ProductTable/Index.vue
+++ components/ProductTable/Index.vue
@@ -2,9 +2,6 @@
     <section>
         <ProductTableMainTable :products="products" ref="mainTable"/>
         <button @click="createPDF">Crear pdf</button>
-        <div ref="imprimible">
-            imprimible xD
-        </div>
     </section>
 </template>
 
@@ -16,41 +13,21 @@
         }
     })
     const mainTable = ref()
-    // const imprimible = ref(null)
-
-    function createPDF(){
-        exportToPDF('my-pdf-file.pdf', mainTable.value.getClone())
-    }
-    function createPDF_(){
-        // let table = document.getElementsByTagName('table')[0].cloneNode(true)
+    
+    async function createPDF(){
         let table = mainTable.value.getClone()
-        let style =  `
-            <style>
-                table {width: 100%;font: 17px Calibri;}
-                table, th, td {border: solid 1px #DDD; border-collapse: collapse;
-                padding: 2px 3px;text-align: center;}
-            </style>
-        `
-        // CREATE A WINDOW OBJECT.
-        let tableWindow = window.open('', '', 'height=700,width=700');
+        table.style.width = "595.28pt" // Para que ocupe todo el ancho.
 
-        tableWindow.document.write(`
-            <!DOCTYPE html>
-            <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>Document</title>
-                </head>
-                <body>
-                </body>
-            </html>
-        `)
-        tableWindow.document.head.innerHTML += style
-        tableWindow.document.body.append(table)
-        
-        tableWindow.print();    // PRINT THE CONTENTS.
-        tableWindow.close(); 	// CLOSE THE CURRENT WINDOW.
+        // https://sidebase.io/nuxt-pdf/getting-started/quick-start
+        await exportToPDF('my-pdf-file.pdf', table, {
+            orientation: 'l',
+            unit: 'pt',
+            format: 'a4',
+            putOnlyUsedFonts: true,
+            floatPrecision: 'smart',
+            compress: true
+        }, {
+            margin: [60, 30, 60, 30] // Para conservar unos margenes.
+        })
     }
 </script>
