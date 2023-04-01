16fee91497d823b23a3ee2ce95d96c907e4c0666
Carlos <carlosmanueldu@gmail.com>
2023-04-01 15:49:10 -0300

    Resolviendo primer rendering de paginas secundarias

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,4 +7,14 @@
     </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        font-size: 62.5%
+    }
+</style>
--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,7 +1,7 @@
 // For Nuxt 3
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { fas, faHouse } from '@fortawesome/free-solid-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
 // This is important, we are going to let Nuxt worry about the CSS
@@ -9,7 +9,7 @@ config.autoAddCss = false
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(fas, fab, faHouse)
+library.add(fas, fab)
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon, {})
