87fee825e3afb4640e38f06a1e0eca7080058a93
Carlos <carlosmanueldu@gmail.com>
2023-04-04 15:55:22 -0300

    Tree Shaking de iconos de Font-Awesome

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,15 +1,55 @@
 // For Nuxt 3
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import {
+        faBars, 
+        faBarsStaggered,
+        faHouse,
+        faInfoCircle,
+        faComments,
+        faCircleCheck,
+        faDumpsterFire,
+        faCirclePlus,
+        faPanorama,
+        faTableCells,
+        faArrowLeft,
+        faArrowRight,
+        faCamera,
+        faCameraRotate,
+        faFileZipper,
+        faFilePdf,
+        faHandPointUp,
+        faCircleXmark
+      } from '@fortawesome/free-solid-svg-icons'
+import { faReadme } from '@fortawesome/free-brands-svg-icons'
 
 // This is important, we are going to let Nuxt worry about the CSS
 config.autoAddCss = false
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(fas, fab)
+// library.add(fas, fab)
+library.add(
+  faBars, 
+  faBarsStaggered,
+  faHouse,
+  faInfoCircle,
+  faComments,
+  faCircleCheck,
+  faDumpsterFire,
+  faCirclePlus,
+  faPanorama,
+  faTableCells,
+  faArrowLeft,
+  faArrowRight,
+  faCamera,
+  faCameraRotate,
+  faFileZipper,
+  faFilePdf,
+  faHandPointUp,
+  faCircleXmark,
+  faReadme
+)
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon, {})
