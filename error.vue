ebb31cb4b239f85e83925358132a73f33f48691b
Carlos <carlosmanueldu@gmail.com>
2023-04-05 04:50:06 -0300

    reduciendo tamaño de pagina de error para moviles

--- error.vue
+++ error.vue
@@ -39,7 +39,7 @@
         justify-content: center;
         align-items: center;
         gap: 30px;
-        font-size: 4.3rem;
+        font-size: 3.8rem;
         .error-icon {
             font-size: 2em;
             color: steelblue;
