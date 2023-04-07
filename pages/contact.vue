1b3a5caee14a7b8631a0b228c2adc38f187cd282
Carlos <carlosmanueldu@gmail.com>
2023-04-07 06:27:21 -0300

    link noopener

--- pages/contact.vue
+++ pages/contact.vue
@@ -6,13 +6,13 @@
         <section>
             <h2>Puedes ver mis proyectos en desarrollo aquí:</h2>
             <p>
-                <a href="https://github.com/carlosmperilla" target="_blank">https://github.com/carlosmperilla</a>
+                <a href="https://github.com/carlosmperilla" target="_blank" rel="noopener">https://github.com/carlosmperilla</a>
             </p>
         </section>
         <section>
             <h2>Repositorio de este proyecto:</h2>
             <p>
-                <a href="https://github.com/carlosmperilla/Quick-Image-Table" target="_blank">https://github.com/carlosmperilla/Quick-Image-Table</a>
+                <a href="https://github.com/carlosmperilla/Quick-Image-Table" target="_blank" rel="noopener">https://github.com/carlosmperilla/Quick-Image-Table</a>
             </p>
         </section>
         <section>
