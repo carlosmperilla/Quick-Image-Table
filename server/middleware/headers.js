93b7c8b8327f807fdcd1efbc9711a100ebc43b41
Carlos <carlosmanueldu@gmail.com>
2023-04-11 06:56:26 -0300

    añadiendo headers x-content-type-options

--- server/middleware/headers.js
+++ server/middleware/headers.js
@@ -1,4 +1,4 @@
 export default defineEventHandler((event) => {
     setHeader(event, 'x-content-type-options', 'nosniff')
-    // event.node.res.setHeader('x-content-type-options', 'nosniff')
+    console.log(event.node.res._headers)
 })
