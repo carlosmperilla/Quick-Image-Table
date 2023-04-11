d6e9cd0193a38b5f2eb398f4c4101dd99794d5d3
Carlos <carlosmanueldu@gmail.com>
2023-04-11 06:32:56 -0300

    .

--- server/middleware/headers.js
+++ server/middleware/headers.js
@@ -1,4 +1,4 @@
 export default defineEventHandler((event) => {
     // setHeader(event, 'X-Content-Type-Options', 'nosniff')
-    event.node.res.setHeader('X-Content-Type-Options', 'nosniff')
+    event.node.res.setHeader('x-content-type-options', 'nosniff')
 })
