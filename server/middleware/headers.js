54567f15c9a11551bb4c694a44c407d8ec63d894
Carlos <carlosmanueldu@gmail.com>
2023-04-11 11:42:46 -0300

    header configuration

--- server/middleware/headers.js
+++ server/middleware/headers.js
@@ -2,11 +2,11 @@
 // Por ello defino el header mediante la configuración de netlify.
 
 export default defineEventHandler((event) => {
+    setResponseHeader(event, 'x-content-type-options', 'nosniff')
     setResponseHeader(event, 'x-frame-options',  'SAMEORIGIN')
     setResponseHeader(event, 'referrer-policy', 'no-referrer')
     setResponseHeader(event, 'permissions-policy', 'camera=self')
     setResponseHeader(event, 'x-permitted-cross-domain-policies', 'none')
     setResponseHeader(event, 'x-xss-protection', '0')
     setResponseHeader(event, 'content-security-policy', 'frame-ancestors none;')
-    setResponseHeader(event, 'x-content-type-options', 'nosniff')
 })
