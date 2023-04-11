8b252e585664ba97b548ee94028f1e301a9e8071
Carlos <carlosmanueldu@gmail.com>
2023-04-11 06:09:38 -0300

    .

--- server/middleware/headers.js
+++ server/middleware/headers.js
@@ -1,20 +1,3 @@
-export default fromNodeMiddleware((req, res) => {
-    console.log(res)
-    console.log(typeof res)
-    console.log(res.headers)
-    console.log(Object.keys(res))
-    console.log(res.headersSent)
-    console.log(res._headers)
-    res._headers = {
-        "X-Frame-Options": "SAMEORIGIN",
-        "X-XSS-Protection": "1; mode=block",
-        "X-Content-Type-Options": "nosniff"
-    }
-    console.log(Object.keys(req))
-    console.log(req.headers)
-    // res.end({
-    //     "X-Frame-Options": "SAMEORIGIN",
-    //     "X-XSS-Protection": "1; mode=block",
-    //     "X-Content-Type-Options": "nosniff"
-    // });
+export default defineEventHandler((event) => {
+    setHeader(event, "X-Content-Type-Options", "nosniff")
 })
