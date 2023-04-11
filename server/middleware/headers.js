33627b2d257440bdc8ac04e818566e5c57f44760
Carlos <carlosmanueldu@gmail.com>
2023-04-11 05:11:25 -0300

    .

--- server/middleware/headers.js
+++ server/middleware/headers.js
@@ -1,3 +1,8 @@
 export default defineEventHandler((event) => {
     console.log('New request: ' + event.node.req.url)
+    event.node.res.set({
+        "X-Frame-Options": "SAMEORIGIN",
+        "X-XSS-Protection": "1; mode=block",
+        "X-Content-Type-Options": "nosniff"
+    })
 })
