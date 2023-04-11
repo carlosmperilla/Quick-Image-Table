b9c501e0a789d0d41545539dd97772f95f7203f8
Carlos <carlosmanueldu@gmail.com>
2023-04-11 08:25:37 -0300

    Añadiendo headers

--- server/middleware/headers.js
+++ server/middleware/headers.js
@@ -1,5 +1,8 @@
 export default defineEventHandler((event) => {
     // setHeader(event, 'x-content-type-options', 'nosniff')
     setResponseHeader(event, 'x-content-type-options', 'nosniff')
+    setResponseHeader(event, 'x-frame-options',  'SAMEORIGIN')
+    setResponseHeader(event, 'referrer-policy', 'no-referrer')
+    setResponseHeader(event, 'permissions-policy', 'camera=self')
     console.log(event.node.res._headers)
 })
